2f9a5663b24198bf14634c120926e7ed25073ecd
Alex <a.mihm@demv.de>
2017-04-22 09:18:15 +0200

    setup

--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!file_exists($file = __DIR__ . '/../vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
-$loader = require $file;
-$loader->add('Conversio\Mail\Tests', __DIR__);
